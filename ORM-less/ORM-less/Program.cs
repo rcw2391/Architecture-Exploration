11f6f03c549803bfa259251b680c4391342eebe0
Ryan Williams <williamsr@coxmanufacturing.com>
2022-12-22 04:13:59 -0600

    Remove dead code

--- ORM-less/ORM-less/Program.cs
+++ ORM-less/ORM-less/Program.cs
@@ -1,9 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-using DatabaseAccess;
+﻿using DatabaseAccess;
 using Models;
 using Repositories.Concrete;
-
-Console.WriteLine("Hello, World!");
 
 string connString = @"Server=(localdb)\MSSQLLocalDB;Database=Architecture-Testing;Integrated Security=true;";
 
